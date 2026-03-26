0943990bd93bf3905dfc73aaff1c5ec9a51c6f68
zaidansari-sudo <zaidansari0515@gmail.com>
2026-03-26 18:14:10 +0530

    //

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000",
+  "https://travelmatefrontend-production.up.railway.app",
+  ],
   credentials: true,
 }));
 app.use(express.json());
